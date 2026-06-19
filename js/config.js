58d68e891ad806961ac0b7782276feba3056bf51
SirWinstonFR <miniprodgaming@gmail.com>
2026-06-19 21:14:59 +0200

    V3

--- js/config.js
+++ js/config.js
@@ -1,8 +1,7 @@
 const CONFIG = {
-  SHEET_ID:        '1IQ7aneTQUAKT8FGb75h0gnHAKw2F0mmePwtN35QabXk',
+  SHEET_ID:        '1zbnly7aZ8AzZlJDsQA5I3hiKAbD2zlpXedvyNcdtsAQ',
   API_KEY:         'AIzaSyCyvTQg8tzasPEvi2-fDtNkO2n6KEcDL-8',
   SHEET_NAME:      'Feuille 1',
   ADMIN_PASSWORD:  'leroy2025',
-  // Collez ici l'URL générée par Apps Script après déploiement :
-  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbw_2rz3F5ACjhSvYut8xzIB_54A3wzoDVirwsB9IlkU1S2X9k_F2pFo9VXhayipNDps/exec',
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzVyE-VoVVtr9iuw3tsW38Iw-lbT_YGlneYlQpRznG9p-PjIfOAWWvNoMhF7FdhUe-Fcg/exec',
 };
